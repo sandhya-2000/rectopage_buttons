a902213fb62c5f243316455bc55ed1e930c6dd9b
sandhya-2000 <54542411+sandhya-2000@users.noreply.github.com>
2021-03-10 20:37:43 +0530

    uploaded files

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button alertButton;
+    private TextView alertTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        alertButton = (Button) findViewById(R.id.AlertButton);
+        alertTextView = (TextView) findViewById(R.id.AlertTextView);
+
+        alertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("Alert Dialogue");
+                builder.setMessage("Hello! I'm an alert dialog");
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        alertTextView.setVisibility(View.VISIBLE);
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        Button btn = (Button) findViewById(R.id.ToastButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Displaying simple Toast message
+                Toast.makeText(getApplicationContext(),
+                        "This a toast message",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
+
+
+
